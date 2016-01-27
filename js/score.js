b68b5c12abb1ae78c1583b276b4234df3f809a6f
Katherine Beame <katherine.beame@gmail.com>
2016-01-27 13:03:19 -0800

    beautified the code

--- js/score.js
+++ js/score.js
@@ -7,7 +7,7 @@ if (parseInt(kittyScore) === 0) {
 } else if (parseInt(kittyScore) >= 1 & parseInt(kittyScore) <= 3 ) {
   printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a dog person';
 } else if (parseInt(kittyScore) >= 3 & parseInt(kittyScore) <= 5) {
-printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a litter box';
+  printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a litter box';
 } else if (parseInt(kittyScore) >= 6 & parseInt(kittyScore) <= 8) {
   printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a cat taking a bath, you aren’t happy but you’re at least a cat.';
 } else if (parseInt(kittyScore) >= 8 & parseInt(kittyScore) <= 10) {
@@ -15,7 +15,7 @@ printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a litter b
 } else if (parseInt(kittyScore) >= 11 & parseInt(kittyScore) <= 13) {
   printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a a cat wearing a holiday sweater';
 } else if (parseInt(kittyScore) >= 14 & parseInt(kittyScore) <= 19) {
-printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a catnip';
+  printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a catnip';
 } else {
   printScore.textContent = 'You got ' + kittyScore + ' kitties. You are a cat magician!';
 };
